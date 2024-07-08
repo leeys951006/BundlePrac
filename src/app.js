32adc5f89e41dfb2c9f9abca3c86103d46dca237
이연승 <leeys951006@gmail.com>
2024-07-08 11:09:13 +0900

    webpack

---
+++ src/app.js
@@ -0,0 +1,4 @@
+const add = require('./module/add.js');
+
+const test = add(1, 2);
+console.log(test);
---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+const path = require('path');
+
+let name = 'app';
+
+module.exports = {
+  entry: `./src/${name}.js`,
+  output: {
+    filename: `${name}.bundle.js`,
+    path: path.resolve(__dirname, 'dist'),
+  },
+};
